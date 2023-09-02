933324da2534b7322e49525ec804875f34f0dcbf
Amine Benzaazaa <Medzaza910@gmail.com>
2023-09-02 13:05:28 +0100

    modification

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -131,7 +131,7 @@ function App() {
             <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
             <RefineSnackbarProvider>
                 <Refine
-                    dataProvider={dataProvider("http://localhost:8080/api/v1")}
+                    dataProvider={dataProvider("https://dashboard-backend-plum.vercel.app/api/v1")}
                     notificationProvider={notificationProvider}
                     ReadyPage={ReadyPage}
                     catchAll={<ErrorComponent />}
